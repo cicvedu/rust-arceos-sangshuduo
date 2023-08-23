faf6a74ddaa0a6b1aec835b9fbc3368ac16c502d
Shuduo Sang <sangshuduo@gmail.com>
2023-08-23 15:05:54 +0800

    fix: a1

--- exercises/cos_arceos/crates/allocator/src/simple.rs
+++ exercises/cos_arceos/crates/allocator/src/simple.rs
@@ -7,17 +7,30 @@ use core::num::NonZeroUsize;
 
 use crate::{AllocResult, BaseAllocator, ByteAllocator};
 
-pub struct SimpleByteAllocator;
+pub struct SimpleByteAllocator {
+    start: usize,
+    end: usize,
+    allocated: usize,
+    pointer: usize,
+}
 
 impl SimpleByteAllocator {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            end: 0,
+            allocated: 0,
+            pointer: 0,
+        }
     }
 }
 
 impl BaseAllocator for SimpleByteAllocator {
     fn init(&mut self, _start: usize, _size: usize) {
-        todo!();
+        self.start = _start;
+        self.end = self.start + _size;
+        self.pointer = self.start;
+        self.allocated = 0;
     }
 
     fn add_memory(&mut self, _start: usize, _size: usize) -> AllocResult {
@@ -27,22 +40,34 @@ impl BaseAllocator for SimpleByteAllocator {
 
 impl ByteAllocator for SimpleByteAllocator {
     fn alloc(&mut self, _layout: Layout) -> AllocResult<NonZeroUsize> {
-        todo!();
+        let size = _layout.size();
+        let result = self.pointer;
+        if self.pointer + size > self.end {
+            Err(crate::AllocError::NoMemory)
+        } else {
+            self.allocated += 1;
+            self.pointer += size;
+            Ok(NonZeroUsize::new(result).unwrap())
+        }
     }
 
     fn dealloc(&mut self, _pos: NonZeroUsize, _layout: Layout) {
-        todo!();
+        if self.allocated > 0 {
+            self.allocated -= 1;
+        } else {
+            self.pointer = self.start;
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!();
+        self.end - self.start
     }
 
     fn used_bytes(&self) -> usize {
-        todo!();
+        self.pointer - self.start
     }
 
     fn available_bytes(&self) -> usize {
-        todo!();
+        self.end - self.pointer
     }
 }
