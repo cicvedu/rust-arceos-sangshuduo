24aa762de21318a5bf888068ed99fc3d4c8e2a4c
Shuduo Sang <sangshuduo@gmail.com>
2023-08-23 16:13:03 +0800

    fix: a2

--- exercises/cos_arceos/exercises/a2/src/main.rs
+++ exercises/cos_arceos/exercises/a2/src/main.rs
@@ -11,7 +11,6 @@ use std::thread;
 static FLAG: AtomicUsize = AtomicUsize::new(0);
 
 
-// I AM NOT DONE
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
@@ -20,6 +19,7 @@ fn main() {
         while FLAG.load(Ordering::Relaxed) < 1 {
             // For cooperative scheduler, we must yield here!
             // For preemptive scheduler, just relaxed! Leave it for scheduler.
+            thread::yield_now();
         }
 
         let _ = FLAG.fetch_add(1, Ordering::Relaxed);
